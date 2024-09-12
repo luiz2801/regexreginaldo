a4fbf5b3aa57b85a161826a992e8c7bcfe5c3c58
Luiz Moreira <137379417+just-a-padawan@users.noreply.github.com>
2024-09-12 12:18:54 -0300

    hn

--- naturais.py
+++ naturais.py
@@ -4,12 +4,13 @@ alfabeto = ("+", "-")
 vazio = "ε"
 
 def numeros(num):
-    pattern = r'^(([+\-]\d*)|ε|([+\-]\d*\.\d))'.format(alfabeto)
-    if re.match(pattern, num):
+    pattern = r'^(([+\-][0-9]*)|ε|([+\-][0-9]*\.[0-9]*))'
+    a = r'^([+\-][0-9]+(\.[0-9]+)?|ε)$'
+    if re.match(a, num):
         return 1
     return 0
 
 
-a = "-5.45"
+a = "-91.922"
 
 print(numeros(a))
